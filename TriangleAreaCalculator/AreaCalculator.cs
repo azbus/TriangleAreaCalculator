264ea20eb153a6bd4be4b0e17d629ee5008676bf
Azbus <onix.09@mail.ru>
2017-07-21 02:46:28 +0300

    Добавьте файлы проекта.

---
+++ TriangleAreaCalculator/AreaCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleAreaCalculator
+{
+    public class AreaCalculator
+    {
+        private AreaCalculator() { }
+        public static double calculateRightTriangleArea(double a, double b)
+        {
+            
+            double area = 0.5 * a * b;
+            return area;
+        }
+
+        public static double calculateAnyTriangleArea(double a, double b, double c)
+        {
+            if (a + b <= c || b + c <= a || c + a <= b)
+                throw new Exception("Такого треугольника не существует!");
+
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+
+    }
+}
---
+++ TriangleAreaCalculatorTests/AreaCalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleAreaCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleAreaCalculator.Tests
+{
+    [TestClass()]
+    public class AreaCalculatorTests
+    {
+        [TestMethod()]
+        public void calculateTriangleAreaWithSides_4_4_4()
+        {
+            double expectedValue = 6.928203230275509;
+
+            double a = 4.0;
+            double b = 4.0;
+            double c = 4.0;
+            double area = AreaCalculator.calculateAnyTriangleArea(a, b, c);
+
+            Assert.AreEqual(expectedValue, area);
+        }
+
+        [TestMethod()]
+        public void calculateRightTriangleAreaWithSides_2_2()
+        {
+            double expectedValue = 2.0;
+
+            double a = 2.0;
+            double b = 2.0;
+            double area = AreaCalculator.calculateRightTriangleArea(a, b);
+
+            Assert.AreEqual(expectedValue, area);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(Exception),
+            "Такого треугольника не существует!")]
+        public void calculateTriangleArea_WithWrongSides()
+        {
+            double a = 40.0;
+            double b = 4.0;
+            double c = 4.0;
+            double area = AreaCalculator.calculateAnyTriangleArea(a, b, c);
+        }
+    }
+}
